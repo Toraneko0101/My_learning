8b6868a483cc2077b1a92dad7c20c6032bb42a8d
Toraneko0101 <nekoblack0101@gmail.com>
2023-11-12 18:01:29 +0900

    to step5

--- introduction_to_compier_creation/compiler/9cc.c
+++ introduction_to_compier_creation/compiler/9cc.c
@@ -94,11 +94,8 @@ Token *new_token(TokenKind kind, Token *cur, char *str){
 // 入力文字列pをトークナイズし、それを返す
 Token *tokenize(){
   char *p = user_input;
-  Token head;//トークンのリストの先頭を指す。head.nextで先頭
+  Token head;
   head.next = NULL;
-  //一回目のloopではculがheadのアドレスを指しているので
-  //head.nextは連結リストの先頭を指す。それ以降は
-  //curはheadのアドレスを指していないので変更されない
   Token *cur = &head; 
   //空白文字のスキップ
   while (*p){
@@ -106,29 +103,130 @@ Token *tokenize(){
       p++;
       continue;
     }
-    if (*p == '+' || *p == '-'){
-      //ポストインクリメントなので、式で使用された後に増加させる
-      //一回目 cur = head 
-      //       cur.next = first_tok
-      //二回目 cur = first_tok 
-      //       cur.next = another_tok
-      //以上のように連結されていく
+    //Punctuator
+    if (strchr("+-*/()", *p)){
       cur = new_token(TK_RESERVED, cur, p++);//トークン作成
       continue;
     }
 
     if(isdigit(*p)){
       cur = new_token(TK_NUM, cur, p);
-      cur -> val = strtol(p, &p, 10); //数値を返す
+      cur -> val = strtol(p, &p, 10); //pは数値の次を指す
       continue;
     }
 
-    error_at(p, "expected a number");
+    error_at(p, "invalid token");
   }
-  //末尾の場合、記号で終わることはないのでpはstrtolで範囲外を示しているはずだ
   new_token(TK_EOF, cur, p);
-  //連結リストの最初の要素アドレスを返す
   return head.next;
+}
+
+//Parser
+typedef enum{
+  ND_ADD,
+  ND_SUB,
+  ND_MUL,
+  ND_DIV,
+  ND_NUM, //Int
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node{
+  NodeKind kind;
+  Node *lhs;
+  Node *rhs;
+  int val;
+};
+
+Node *new_node(NodeKind kind){
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  return node;
+}
+
+Node *new_binary(NodeKind kind, Node *lhs, Node *rhs){
+  Node *node = new_node(kind);
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_num(int val){
+  Node *node = new_node(ND_NUM);
+  node->val = val;
+  return node;
+}
+
+Node *expr();
+Node *mul();
+Node *primary();
+
+Node *expr(){
+  Node *node = mul();
+  //値を返す(+,-が来なくなったら終わり)
+  for(;;){
+    if(consume('+'))
+      node = new_binary(ND_ADD, node, mul());
+    else if(consume('-'))
+      node = new_binary(ND_SUB, node, mul());
+    else
+      return node;
+  }
+}
+
+Node *mul(){
+  Node *node = primary();
+
+  for(;;)
+    if(consume('*'))
+      node = new_binary(ND_MUL, node, primary());
+    else if(consume('/'))
+      node = new_binary(ND_DIV, node, primary());
+    else
+      return node;
+}
+
+Node *primary(){
+  if(consume('(')){
+    Node *node = expr();
+    expect(')');
+    return node;
+  }
+
+  return
+  new_num(expect_number());
+}
+
+void gen(Node *node){
+  if(node->kind == ND_NUM){
+    printf("  push %d\n", node->val);
+    return;
+  }
+
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+
+  switch (node->kind){
+    case ND_ADD:
+      printf("  add rax, rdi\n");
+      break;
+    case ND_SUB:
+      printf("  sub rax, rdi\n");
+      break;
+    case ND_MUL:
+      printf("  imul rax, rdi\n");
+      break;
+    case ND_DIV:
+      printf("  cqo\n");
+      printf("  idiv rdi\n");
+      break;
+  }
+
+  printf("  push rax\n");
 }
 
 int main(int argc, char **argv){
@@ -137,30 +235,21 @@ int main(int argc, char **argv){
     error_at("%s: invalid number of arguments", argv[0]);
     return 1;
   }
-  //トークナイズする
+  //記号と数値を分離
   user_input = argv[1];
   token = tokenize();
+  Node *node = expr();
 
   //アセンブリの前半部分を出力
   printf(".intel_syntax noprefix\n");
   printf(".globl main\n");
   printf("main:\n");
 
-  //式の最初は数である必要があるのでチェック
-  //最初のmov命令を出力
-  printf("  mov rax, %d\n", expect_number());
+  //抽象構文木を下りながらコード生成
+  gen(node);
 
-  // -<num> +<num>という並びを消化する
-  while (!at_eof()){
-    if (consume('+')){
-      printf("  add rax, %d\n", expect_number());
-      continue;
-    }
-    //もしマイナスでもなければerror
-    expect('-');
-    printf("  sub rax, %d\n", expect_number());
-  }
 
+  printf("  pop rax\n");
   printf("  ret\n");
   return 0;
 }
